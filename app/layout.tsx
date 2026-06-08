4e1abd26fcc83d76b6dc39476f23b5a8558f7dcf
shady-gif <sarrthakcsingh@gmail.com>
2026-06-08 21:45:51 +0530

    ad

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -30,7 +31,23 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        {children}
+
+        <Script id="monetag-inpage-push" strategy="afterInteractive">
+          {`
+            (function(s){
+              s.dataset.zone='11119426';
+              s.src='https://nap5k.com/tag.min.js';
+            })(
+              [document.documentElement, document.body]
+                .filter(Boolean)
+                .pop()
+                .appendChild(document.createElement('script'))
+            );
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
