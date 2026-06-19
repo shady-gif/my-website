103e6ee786aa39be04687c97ee97ff551b4af484
shady-gif <sarrthakcsingh@gmail.com>
2026-06-19 13:32:07 +0530

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -40,6 +41,13 @@ export default function RootLayout({
 
         <Analytics />
         <GoogleAnalytics gaId="G-9ZJG4CCERV" />
+        <script type="text/javascript">
+        (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+         })(window, document, "clarity", "script", "x9dn58skrw");
+         </script>
       </body>
     </html>
   );
